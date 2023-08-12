25f27f5a42e2af484e28be99aa1bec02a80c81bf
sina rasouli <sinarasouli79@gmail.com>
2023-08-12 16:10:52 +0330

    implement rules for borrow endpoint

--- library/serializers.py
+++ library/serializers.py
@@ -1,9 +1,57 @@
+import datetime
+
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
-from library.models import Borrow
+from library.models import Borrow, Collection
 
 
 class CreateBorrowSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        book = validated_data.get('book')
+        collection = get_object_or_404(Collection, pk=book.collection.pk)
+        customer = validated_data.get('customer')
+
+        if customer.is_ban:
+            raise serializers.ValidationError({'customer': 'this is a ban customer'})
+
+        if customer.balance < (collection.borrow_price * 3):
+            raise serializers.ValidationError({'customer': 'customer balance is low.'})
+
+        # check for book inventory
+        if book.borrow_inventory == 0:
+            raise serializers.ValidationError({'book': 'borrow inventory is low'})
+
+        if Borrow.objects.filter(customer=customer, book__collection=collection).count() == collection.borrow_limit:
+            raise serializers.ValidationError(f"customer brake the borrow limit rule for collection\
+                 '{collection.title}({collection.borrow_limit})' .")
+
+        expected_return_date = validated_data.get('expected_return_date')
+        inventory = book.buy_inventory  # referred as n in the document
+        today = datetime.datetime.today()
+        last_30_days = today - datetime.timedelta(days=30)
+        borrowed_in_last_30_days = Borrow.objects.filter(book=book,
+                                                         borrow_date__lte=today,
+                                                         borrow_date__gt=last_30_days
+                                                         ).count()
+
+        if borrowed_in_last_30_days < 3:
+            borrowed_in_last_30_days = 3
+
+        max_borrow_date = (((30 * inventory) // inventory) + borrowed_in_last_30_days) + 1
+
+        today = datetime.date.today()
+        subtract = expected_return_date - today
+        if subtract.days > max_borrow_date:
+            raise serializers.ValidationError(
+                {'expected_return_date': f'invalid return date(max return date {max_borrow_date})'})
+
+        # update book inventory
+        book.borrow_inventory -= 1
+        book.save()
+        return super().create(validated_data)
+
     class Meta:
         model = Borrow
         fields = ['id', 'book', 'customer', 'borrow_date', 'expected_return_date', 'actual_return_date']
--- library/tests/test_borrow.py
+++ library/tests/test_borrow.py
@@ -12,11 +12,13 @@ class TestCreateBorrow:
         def _create_borrow(is_anonymous=False, is_librarian=False, data=None):
             authenticate(is_anonymous=is_anonymous, is_librarian=is_librarian)
             book = baker.make('library.Book')
-            customer = baker.make('library.Customer')
+            min_customer_balance = book.collection.borrow_price * 3
+            customer = baker.make('library.Customer', balance=min_customer_balance)
 
             if not data:
+                tomorrow = datetime.date.today() + datetime.timedelta(1)
                 data = {'book': book.id, 'customer': customer.id,
-                        'expected_return_date': datetime.datetime.now(),
+                        'expected_return_date': tomorrow,
                         }
 
             return api_client.post('/library/borrow/', data=data)
@@ -33,4 +35,97 @@ class TestCreateBorrow:
 
     def test_if_user_is_librarian_and_data_is_valid_return_201(self, create_borrow):
         response = create_borrow(is_librarian=True)
+        print(response.data)
         assert response.status_code == status.HTTP_201_CREATED
+
+    def test_if_user_brakes_collection_borrow_limit_rule_return_400(self, create_borrow):
+        collection = baker.make('library.Collection', borrow_limit=3)
+        books = baker.make('library.Book', collection=collection, _quantity=4)
+        customer = baker.make('library.Customer')
+        # already_borrowed_book = baker.make('library.borrow', collection=collection)
+        for book in books[:3]:
+            baker.make('library.Borrow', book_id=book.id, customer=customer)
+
+        #  borrowing this book bakes the rule because the borrow limit of the collection is 3
+        #  and the customer already borrowed 3 books.
+        new_borrow_book = baker.make('library.Book', collection=collection)
+        tomorrow = datetime.date.today() + datetime.timedelta(1)
+        data = {'book': new_borrow_book.id, 'customer': customer.id,
+                'expected_return_date': tomorrow
+            ,
+                }
+
+        response = create_borrow(is_librarian=True, data=data)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_if_user_brakes_maximum_borrow_date_return_400(self, create_borrow):
+        # arrange
+        book = baker.make('library.book', borrow_inventory=2)
+        min_customer_balance = book.collection.borrow_price * 3
+        customer = baker.make('library.Customer', balance=min_customer_balance)
+        print(customer.id)
+        tomorrow = datetime.date.today() + datetime.timedelta(1)
+        data = {
+            'book': book.id,
+            'customer': customer.id,
+            'expected_return_date': tomorrow,
+        }
+
+        customer = baker.make('library.Customer', balance=min_customer_balance)
+        create_borrow(is_librarian=True, data=data)
+        next_year = datetime.date.today() + datetime.timedelta(days=365)
+        data = {
+            'book': book.id,
+            'customer': customer.id,
+            'expected_return_date': next_year
+        }
+        response = create_borrow(is_librarian=True, data=data)
+        print(response.data)
+        # assert False
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['expected_return_date'] is not None
+
+    def test_if_book_inventory_is_low_return_400(self, create_borrow):
+        book = baker.make('library.Book', borrow_inventory=0)
+        min_customer_balance = book.collection.borrow_price * 3
+        customer = baker.make('library.Customer', balance=min_customer_balance)
+        tomorrow = datetime.date.today() + datetime.timedelta(1)
+        data = {
+            'book': book.id,
+            'customer': customer.id,
+            'expected_return_date': tomorrow,
+        }
+        response = create_borrow(is_librarian=True, data=data)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['book'] is not None
+
+    def test_if_customer_is_ban_return_400(self, create_borrow):
+        book = baker.make('library.Book', borrow_inventory=1)
+        min_customer_balance = book.collection.borrow_price * 3
+        customer = baker.make('library.Customer', balance=min_customer_balance, is_ban=True)
+        tomorrow = datetime.date.today() + datetime.timedelta(1)
+        data = {
+            'book': book.id,
+            'customer': customer.id,
+            'expected_return_date': tomorrow,
+        }
+        response = create_borrow(is_librarian=True, data=data)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['customer'] is not None
+
+    def test_if_customer_balance_is_low_return_400(self, create_borrow):
+        book = baker.make('library.Book', borrow_inventory=1)
+        min_customer_balance = book.collection.borrow_price * 3
+        customer = baker.make('library.Customer', balance=min_customer_balance - 1)
+        tomorrow = datetime.date.today() + datetime.timedelta(1)
+        data = {
+            'book': book.id,
+            'customer': customer.id,
+            'expected_return_date': tomorrow,
+        }
+        response = create_borrow(is_librarian=True, data=data)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['customer'] is not None
+   
